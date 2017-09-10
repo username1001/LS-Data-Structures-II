2ff4b0263cc1731de297a91bc74ce7de254805b9
Matt <Faymatt96@gmail.com>
2017-09-10 03:54:30 -0400

    binary-search-tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,10 +1,37 @@
 // https://msdn.microsoft.com/en-us/library/aa289150(v=vs.71).aspx
-class BinarySearchTree {
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
-  }
+function Node(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
 }
+function BinarySearchTree() {
+  this.root = null;
+}
+BinarySearchTree.prototype.push = (val) => {
+  const root = this.root;
 
+  if (!root) {
+    this.root = new Node(val);
+    return;
+  }
+  let currentNode = root;
+  const newNode = new Node(val);
+
+  while (currentNode) {
+    if (val < currentNode.value) {
+      if (!currentNode.left) {
+        currentNode.left = newNode;
+        break;
+      } else {
+        currentNode = currentNode.left;
+      }
+    }
+    if (!currentNode.right) {
+      currentNode.right = newNode;
+      break;
+    } else {
+      currentNode = currentNode.right;
+    }
+  }
+};
 module.exports = BinarySearchTree;
